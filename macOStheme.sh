554a6525d67c21139a1869171316a09a0fe0a3cf
w86f <w86f9x@gmail.com>
2020-05-27 18:17:10 -0700

    update

--- macOStheme.sh
+++ macOStheme.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/paullinuxthemer/Mc-OS-themes.git
-mkdir -p $HOME/.theme/
+mkdir -p $HOME/.themes/
 cd Mc-OS-themes
-cp -r * $HOME/.theme/
+cp -r * $HOME/.themes/
 cd ..
 sudo rm -r Mc-OS-themes
