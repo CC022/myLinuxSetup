32d30a5c4ae304e889e529a8abf20ddd5ce738e9
CC022 <w86f9x@gmail.com>
2020-08-13 10:29:46 -0700

    Update dracula.sh
    +fish

--- dracula.sh
+++ dracula.sh
@@ -1,4 +1,6 @@
 cd dracula
 mkdir -p $HOME/.local/share/gedit/styles/
 cp dracula.xml $HOME/.local/share/gedit/styles/
+mkdir -p ~/.config/fish/conf.d/
+cp dracula.fish ~/.config/fish/conf.d/
 ./install.sh
